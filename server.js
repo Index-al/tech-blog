2c12d6fd7545752491ef7af61c8219207ee3a5bd
Indexal <148807519+Index-al@users.noreply.github.com>
2024-02-15 15:02:44 -0600

    Added server.js logic and a couple test views

---
+++ server.js
@@ -0,0 +1,48 @@
+// Purpose: This file is the entry point for the application. It sets up the server and the connection to the database. It also sets up the session and the handlebars engine. It also sets up the routes for the application.
+
+// Dependencies
+const path = require("path"); // Path module
+const express = require("express"); // Express module
+const session = require("express-session"); // Express session module
+const exphbs = require("express-handlebars"); // Express handlebars module
+const routes = require("./controllers"); // Routes module
+const helpers = require("./utils/helpers"); // Helpers module
+const sequelize = require("./config/connection"); // Connection module
+const SequelizeStore = require("connect-session-sequelize")(session.Store); // Session store module
+
+// Variables
+const app = express();
+const PORT = process.env.PORT || 3001; 
+const hbs = exphbs.create({ helpers });
+const sess = {
+  secret: process.env.SESS_SECRET,
+  cookie: {
+    maxAge: 3 * 60 * 60 * 1000,
+    httpOnly: true,
+    secure: false,
+    sameSite: "strict",
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+// Middleware
+app.use(session(sess));
+// Handlebars
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+// Express
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+// Routes
+app.use(routes);
+// Connection
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () =>
+    console.log(`Now listening at http://localhost:${PORT}`)
+  );
+});
