275d8415491a56fef2ec86363952224a4d0ed506
Indexal <148807519+Index-al@users.noreply.github.com>
2024-02-15 14:37:12 -0600

    Added logout functionality

---
+++ public/js/logout.js
@@ -0,0 +1,21 @@
+// Logout functionality
+const logout = async () => {
+    const response = await fetch("/api/users/logout", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" }
+    });
+
+    if (response.ok) {
+        document.location.replace("/");
+    } else {
+        // In the event of an error, report the error
+        const data = await response.json();
+        console.log(data);
+
+        // TODO: Add a modal or some other way to display the error instead of an alert
+        alert("Failed to log out.");
+    }
+}
+
+// Event listener for logout button
+document.querySelector("#logout").addEventListener("click", logout);
